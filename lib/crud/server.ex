622213acaf528c39de84b8724342817de352fac1
Oleg G.Kapranov <lugatex@yahoo.com>
2019-01-11 18:45:59 +0200

    added GenServer - server.ex

--- lib/crud/server.ex
+++ lib/crud/server.ex
@@ -1,3 +1,11 @@
 defmodule Crud.Server do
   @moduledoc false
+
+  use GenServer
+
+  @name __MODULE__
+
+  def start_link(opts), do: GenServer.start_link(@name, :ok, opts)
+
+  def init(:ok), do: {:ok, []}
 end
