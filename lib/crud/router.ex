0e80eebaae4c60eb7eafc58062b73bc6d7f7c710
Oleg G.Kapranov <lugatex@yahoo.com>
2019-01-11 20:32:57 +0200

    added template and route

--- lib/crud/router.ex
+++ lib/crud/router.ex
@@ -2,13 +2,18 @@ defmodule Crud.Router do
   @moduledoc false
 
   use Plug.Router
+  require  EEx
 
+  @template "lib/crud/index.html.eex"
+
+  plug(Plug.Static, from: :crud, at: "/static")
   plug(Plug.Logger)
   plug(:match)
   plug(:dispatch)
 
   get "/" do
-    send_resp(conn, 200, "Hello, world!")
+    response =  EEx.eval_file(@template)
+    send_resp(conn, 200, response)
   end
 
   match(_, do: send_resp(conn, 404, "This is not the page you are looking for"))
