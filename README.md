ddfec9ad1ed14ac30c364597fae0c00b630aef69
Oleg G.Kapranov <lugatex@yahoo.com>
2019-01-11 18:40:48 +0200

    added Makfile and analyse packages

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CRUD OTP, HTTP Application without framework.
 
+## Setup a project
+
 ```bash
 bash> mkdir crud-otp-http; cd crud-otp-http
 bash> mix new ./ --app crud --sup
--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,15 @@
 use Mix.Config
 
-# import_config "#{Mix.env()}.exs"
+if Mix.env == :dev do
+  config :mix_test_watch,
+    clear: true
+
+  config :remix,
+    escript: true,
+    silent: true
+end
+
+if Mix.env == :test do
+  config :ex_unit_notifier,
+    notifier: ExUnitNotifier.Notifiers.NotifySend
+end
