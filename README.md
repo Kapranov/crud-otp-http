a990c38a62e194d229766c6042a53056e705f098
Oleg G.Kapranov <lugatex@yahoo.com>
2019-01-11 18:26:22 +0200

    initial a project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# crud-otp-http
+# CRUD OTP, HTTP Application without framework.
+
+```bash
+bash> mkdir crud-otp-http; cd crud-otp-http
+bash> mix new ./ --app crud --sup
+```
+
+### 11 Jan 2019 by Oleg G.Kapranov
---
+++ lib/crud.ex
@@ -0,0 +1,18 @@
+defmodule Crud do
+  @moduledoc """
+  Documentation for Crud.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Crud.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
