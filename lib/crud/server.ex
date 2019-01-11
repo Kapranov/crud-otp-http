bde9b0c12b83cc4bd7293339d005ab6cf5d39553
Oleg G.Kapranov <lugatex@yahoo.com>
2019-01-11 20:03:11 +0200

    added toggle - server.ex, crud_test.exs

--- lib/crud/server.ex
+++ lib/crud/server.ex
@@ -14,25 +14,18 @@ defmodule Crud.Server do
 
       iex> Crud.Server.show
       []
-
   """
   def show, do: GenServer.call(@name, {:show})
 
   @doc """
   Add new item
-
-  ## Example
-
-      iex> Crud.Server.add("hello")
-      [
-        %{
-          done: false,
-          id: generate_id()
-          name: "hello"
-        }
-      ]
   """
   def add(item), do: GenServer.call(@name, {:add, item})
+
+  @doc """
+  Toggle change done on true or false
+  """
+  def toggle(id), do: GenServer.call(@name, {:toggle, id})
 
   @impl true
   def init(:ok), do: {:ok, []}
@@ -50,9 +43,28 @@ defmodule Crud.Server do
     {:reply, new_items, new_items}
   end
 
+  @impl true
+  def handle_call({:toggle, id}, _from, items) do
+    [item] = Enum.filter(items, fn x -> x.id == id end)
+    toggled_item = %{item |  done:  !item.done}
+    new_items =
+      items
+      |> Enum.map(fn x ->
+          if is_id?(x, id) do
+            toggled_item
+          else
+            x
+          end
+      end)
+
+    {:reply, new_items, new_items}
+  end
+
   defp generate_id do
     :crypto.strong_rand_bytes(64)
     |> Base.url_encode64()
     |> binary_part(0,  64)
   end
+
+  defp is_id?(x, id), do: x.id == id
 end
--- test/crud_test.exs
+++ test/crud_test.exs
@@ -1,6 +1,6 @@
 defmodule CrudTest do
   use ExUnit.Case
-  doctest Crud
+  doctest Crud.Server
 
   test "show items" do
     items = Crud.Server.show
@@ -8,10 +8,16 @@ defmodule CrudTest do
     assert items == []
   end
 
-  test "add item" do
-    item = Crud.Server.add("hello")
-    [%{name: value} | _others] = item
+  test "add, toggle item" do
+    [item] = Crud.Server.add("hello")
+    value1 = Crud.Server.toggle(item.id)
+    [%{done: value} | _others] = value1
 
-    assert value == "hello"
+    assert value == true
+
+    value2 = Crud.Server.toggle(item.id)
+    [%{done: value} | _others] = value2
+
+    assert value == false
   end
 end
